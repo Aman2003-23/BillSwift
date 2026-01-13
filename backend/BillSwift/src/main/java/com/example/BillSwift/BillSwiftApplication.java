33bd4b24154794b8fa9642e2696227245e65f26c
Aman <amanagarwal0723@gmail.com>
2026-01-13 16:47:18 +0530

    project setup

---
+++ backend/BillSwift/src/main/java/com/example/BillSwift/BillSwiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.BillSwift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSwiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillSwiftApplication.class, args);
+	}
+
+}
---
+++ backend/BillSwift/src/test/java/com/example/BillSwift/BillSwiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BillSwift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillSwiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

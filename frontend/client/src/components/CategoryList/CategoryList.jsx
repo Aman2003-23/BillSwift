70d8d6b861a83e4f3afa55e17218a6a8e4c2d73a
Aman <amanagarwal0723@gmail.com>
2026-01-20 21:06:22 +0530

    designing

--- frontend/client/src/components/CategoryList/CategoryList.jsx
+++ frontend/client/src/components/CategoryList/CategoryList.jsx
@@ -1,7 +1,38 @@
+import React from 'react';
+import "./CategoryList.css";
+import { useContext } from 'react';
+import { AppContext } from '../../Context/AppContext';
+
 const CategoryList=()=>{
+    const {categories}=useContext(AppContext);
     return(
-        <div>
-            <h1>Welcome to the Category List Page</h1>
+        <div className="category-list-container" style={{height: '100vh', overflowY: 'auto', overFlowX:'hidden'}}>
+            <div className="row pe-2">
+                search bar
+            </div>
+            <div className="row g-3 pe-2">
+               {categories.map((category,index)=>(
+                  <div key={index} className="col12"> 
+                      <div  className="card p-3" style={{backgroundColor: category.bgColor}}>
+                          <div className="d-flex align-items-center">
+                               <div style={{marginRight:'15px'}}>
+                                <img src={category.imgUrl} alt={category.name} className="category-image"/>
+                               </div>
+                               <div className="flex-grow-1">
+                                <h5 className="mb-1 text-white">{category.name}</h5>
+                                <p className="mb-0 text-white">5 items</p>
+                               </div>
+                               <div>
+                                <button className="btn btn-danger btn-sm">
+                                    <i className="bi bi-trash"></i>
+                                </button>
+                               </div>
+                           </div>
+                      </div>
+                  </div>
+                ))}
+            </div>
+
         </div>
     )
 }
--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -6,11 +6,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter } from 'react-router-dom';
+import { AppContextProvider } from './Context/AppContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   
     <BrowserRouter>
+    <AppContextProvider>
       <App />
+      </AppContextProvider>
     </BrowserRouter>
  
 )
